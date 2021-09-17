75b617b8961db4509c38661f8a1ca097a54e16db
dengshishuang <2353926228@qq.com>
2021-09-17 10:19:51 +0800

    Desc:2021/0917 基础模块提交 Remark:NA

--- common/src/main/java/com/yuji/xlt/common/constants/ApiConstants.kt
+++
@@ -1,7 +0,0 @@
-package com.yuji.xlt.common.constants
-
-
-object ApiConstants {
-    const val BASE_URL = "https://www.wanandroid.com"
-    const val PAGE_SIZE = 20
-}
--- main/src/main/java/com/yuji/xlt/main/demo/data/model/UserModel.kt
+++
@@ -1,9 +0,0 @@
-package com.yuji.xlt.main.demo.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class UserModel(
-    val userId: String,
-    val displayName: String
-)
